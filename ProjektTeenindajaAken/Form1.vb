9ff3086a65c07537c25d115718e5a6b2e66aaad2
vashep <valshepzolanski@gmail.com>
2022-01-25 13:10:11 +0200

    function

--- ProjektTeenindajaAken/Form1.vb
+++ ProjektTeenindajaAken/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
 
+    Private Const KM2MI = 1.609344
+    Private Sub btnTeisenda_Click(sender As Object, e As EventArgs) Handles btnTeisenda.Click
+        Try
+            'proovime teisendada
+            txtMiilid.Text = teisenda(txtKilomeetrid.Text)
+
+        Catch ex As Exception
+            'vea korral toome ette hoiatuskasti
+            MsgBox("Viga sisendis!")
+
+        End Try
+    End Sub
+
+    Function teisenda(ByVal km As Double) As Double
+        Return km / KM2MI
+    End Function
+
 End Class
